2aa95601a4b915492323ce8e0c523f8caf6e0d8f
Kamal Sulaiman <kamal.sulaiman@paynet.my>
2022-11-30 11:50:55 +0800

    Adding fixes for reverse lookup scripts

--- CSV/readCsv.py
+++ CSV/readCsv.py
@@ -1,9 +1,10 @@
+#Reverse lookup script
+
 import pandas as pd
 import socket
-data = pd.read_csv("switches.csv")
+data = pd.read_csv("data/switches.csv")
 # hostname = data['Hostname'].astype(str) + ".example.domain]"
 
 for i in range(len(data)):
     ipAddr = data.loc[i, 'IP Address']
-    #print(socket.gethostbyaddr(ipAddr)[0], socket.gethostbyaddr(ipAddr)[2])
-    print(socket.gethostbyaddr(ipAddr)[0])
+    print(socket.gethostbyaddr(ipAddr)[0], socket.gethostbyaddr(ipAddr)[2])
